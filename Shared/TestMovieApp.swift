0688ade1529eee08d895ff89ee8c94d65497c5ba
singgih <singgihnn@gmail.com>
2020-10-15 23:07:25 +0700

    Initial Commit

---
+++ Shared/TestMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMovieApp.swift
+//  Shared
+//
+//  Created by singgih on 15/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TestMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
